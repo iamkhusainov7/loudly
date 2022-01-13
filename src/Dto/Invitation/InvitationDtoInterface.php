603fb9ef15c9a97f309d65cc57f7b7b7ea68f223
iamkhusainov7 <=>
2022-01-13 23:49:58 +0100

    fix constatns

--- src/Dto/Invitation/InvitationDtoInterface.php
+++ src/Dto/Invitation/InvitationDtoInterface.php
@@ -6,9 +6,10 @@ interface InvitationDtoInterface
 {
     public const INVITATION_Id_KEY = 'invitationId';
     public const INVITATION_UPDATED_AT = 'invitationUpdatedAt';
-    public const INVITATION_INVITED_AT = 'invitationAt';
+    public const INVITATION_INVITED_AT = 'invitationInvitedAt';
     public const INVITATION_IS_CANCELED = 'invitationIsCanceled';
-    public const INVITATION_IS_ACCEPTED = 'isAccepted';
+    public const INVITATION_IS_ACCEPTED = 'invitationIsAccepted';
+    public const INVITATION_IS_DECLINED = 'invitationIsDeclined';
     public const INVITATION_INVITED_BY = 'invitationInvitedBy';
     public const INVITATION_USER_INVITED = 'invitationInvitedUser';
 }
